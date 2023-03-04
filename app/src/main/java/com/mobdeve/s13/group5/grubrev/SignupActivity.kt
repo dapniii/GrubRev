84f1e8bf5a8ae66855aaf1242c84cf81ee4c5815
spensur <daniel_spencer_tan@dlsu.edu.ph>
2023-03-04 18:28:25 +0800

    Added Signup Page

---
+++ app/src/main/java/com/mobdeve/s13/group5/grubrev/SignupActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s13.group5.grubrev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
