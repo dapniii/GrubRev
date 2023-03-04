edb1aa66b142d0b26e99ac4987636c661ffc9a95
spensur <daniel_spencer_tan@dlsu.edu.ph>
2023-03-04 19:09:51 +0800

    Linked activities
    - Clicking 'Login here' now redirects to SignupActivity
    - Clicking 'Sign up now' now redirects to MainActivity (LoginActivity)

--- app/src/main/java/com/mobdeve/s13/group5/grubrev/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s13/group5/grubrev/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.mobdeve.s13.group5.grubrev
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+//  Instantiate Variables
+    private lateinit var usernameEt : EditText
+    private lateinit var passwordEt : EditText
+    private lateinit var loginBtn : Button
+    private lateinit var signupTv : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Link Variables to xml Components
+        this.usernameEt = findViewById(R.id.usernameEt)
+        this.passwordEt = findViewById(R.id.passwordEt)
+        this.loginBtn = findViewById(R.id.loginBtn)
+        this.signupTv = findViewById(R.id.signupTv)
+
+        //OnClick "Sign up now!" TextView
+        this.signupTv.setOnClickListener(View.OnClickListener {
+            openSignUpActivity()
+        })
+    }
+
+    //Opens SignupActivity and finishes MainActivity
+    private fun openSignUpActivity() {
+        val intent = Intent(this, SignupActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/mobdeve/s13/group5/grubrev/SignupActivity.kt
+++ app/src/main/java/com/mobdeve/s13/group5/grubrev/SignupActivity.kt
@@ -1,11 +1,42 @@
 package com.mobdeve.s13.group5.grubrev
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class SignupActivity : AppCompatActivity() {
+//  Instantiate Variables
+    private lateinit var usernameEt : EditText
+    private lateinit var passwordEt : EditText
+    private lateinit var confirmPasswordEt : EditText
+    private lateinit var signupBtn : Button
+    private lateinit var loginTv : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        //Link Variables to xml Components
+        this.usernameEt = findViewById(R.id.usernameEt)
+        this.passwordEt = findViewById(R.id.passwordEt)
+        this.confirmPasswordEt = findViewById(R.id.confirmPasswordEt)
+        this.signupBtn = findViewById(R.id.signupBtn)
+        this.loginTv = findViewById(R.id.loginTv)
+
+        //OnClick "Login here!" TextView
+        this.loginTv.setOnClickListener(View.OnClickListener {
+            openMainActivity()
+        })
+    }
+
+    //Opens MainActivity and finishes SignupActivity
+    private fun openMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
