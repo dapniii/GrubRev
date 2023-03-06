f9d8f4169598ec90ce3e80d873ece5c08adb1808
spensur <daniel_spencer_tan@dlsu.edu.ph>
2023-03-06 22:43:21 +0800

    Update MyReviewViewHolder.kt

--- app/src/main/java/com/mobdeve/s13/group5/grubrev/MyReviewViewHolder.kt
+++ app/src/main/java/com/mobdeve/s13/group5/grubrev/MyReviewViewHolder.kt
@@ -1,11 +1,18 @@
 package com.mobdeve.s13.group5.grubrev
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 
 class MyReviewViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-    fun bindData(review: Review) {
+    private val userTv: TextView = itemView.findViewById(R.id.userTv)
+    private val commentTv: TextView = itemView.findViewById(R.id.commentTv)
+    private val ratingTv: TextView = itemView.findViewById(R.id.ratingTv)
 
+    fun bindData(review: Review) {
+        userTv.text = review.user
+        commentTv.text = review.comment
+        ratingTv.text = review.rating.toString()
     }
 }
