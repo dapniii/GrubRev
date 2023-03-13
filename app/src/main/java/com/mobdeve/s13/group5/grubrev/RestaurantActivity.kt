1f6ef7f0cdec4614366247ec3d40fb146a21180c
spensur <daniel_spencer_tan@dlsu.edu.ph>
2023-03-13 23:10:30 +0800

    Added Restaurant Page
    - added restaurant page
    - made username in review container clickable

---
+++ app/src/main/java/com/mobdeve/s13/group5/grubrev/RestaurantActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s13.group5.grubrev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RestaurantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant)
+    }
+}
