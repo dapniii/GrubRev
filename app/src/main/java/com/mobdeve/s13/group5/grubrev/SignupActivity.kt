5e0f927b1ed6013f47f2fe98ac9c11224265bab8
spensur <daniel_spencer_tan@dlsu.edu.ph>
2023-03-30 01:57:30 +0800

    Added another condition for error checking
    - added checker if username already exists

--- app/src/main/java/com/mobdeve/s13/group5/grubrev/SignupActivity.kt
+++ app/src/main/java/com/mobdeve/s13/group5/grubrev/SignupActivity.kt
@@ -1,8 +1,10 @@
 package com.mobdeve.s13.group5.grubrev
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -10,6 +12,7 @@ import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -57,19 +60,19 @@ class SignupActivity : AppCompatActivity() {
             val password = passwordEt.text.toString()
             val confirmPassword = confirmPasswordEt.text.toString()
 
-            val errorMessage = isError(email, username, password, confirmPassword)
-
-            //If there are errors, show corresponding error message
-            if (errorMessage != null) {
-                Toast.makeText(
-                    this,
-                    "ERROR: $errorMessage",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            //Otherwise, approve signup, notify user, and redirect back to Login Activity
-            else {
-                createAccount(email, username, password)
+            isError(email, username, password, confirmPassword) { errorMessage ->
+                //If there are errors, show corresponding error message
+                if (errorMessage != null) {
+                    Toast.makeText(
+                        this,
+                        "ERROR: $errorMessage",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                //Otherwise, approve signup, notify user, and redirect back to Login Activity
+                else {
+                    createAccount(email, username, password)
+                }
             }
         }))
     }
@@ -81,36 +84,43 @@ class SignupActivity : AppCompatActivity() {
         finish()
     }
 
-    private fun isError(
-        email: String,
-        username: String,
-        password: String,
-        confirmPassword: String
-    ): String? {
+    private fun isError( email: String,
+                         username: String,
+                         password: String,
+                         confirmPassword: String,
+                         callback: (String?) -> Unit ) {
         //Check if all fields are filled up
         if (email.isNullOrBlank() ||
             username.isNullOrBlank() ||
             password.isNullOrBlank() ||
             confirmPassword.isNullOrBlank()
         ) {
-            return "Please fill up all fields"
+            callback("Please fill up all fields")
         }
         //Check if password is more than or equal 8 characters
         else if (password.length < 8) {
-            return "Password must be more than 8 characters"
+            callback("Password must be more than 8 characters")
         }
         //Check if Password matches with Confirm Password
         else if (password != confirmPassword) {
-            return "Passwords do not match"
-        } else {
-            return null
+            callback("Passwords do not match")
+        }
+        //Check if Username already exists
+        else {
+            checkUserExists(username) { exists ->
+                if (exists) {
+                    callback("Username is already taken")
+                } else {
+                    callback(null)
+                }
+            }
         }
     }
 
     //Create New Account
     /*TODO:
        1. Check if username taken
-       2. fix error messages, dont show raw firebase error messages
+       2. DONE - fix error messages, dont show raw firebase error messages
      */
     private fun createAccount(email: String, username: String, password: String) {
         //Create new account with email and password
@@ -148,5 +158,17 @@ class SignupActivity : AppCompatActivity() {
                 Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun checkUserExists(username: String, callback: (Boolean) -> Unit) {
+        firebaseDb.collection("users")
+            .whereEqualTo("username", username)
+            .get()
+            .addOnSuccessListener {document ->
+                callback(!document.isEmpty)
+            }
+            .addOnFailureListener {error ->
+                Log.d(ContentValues.TAG, "ERROR: $error")
+            }
     }
 }
